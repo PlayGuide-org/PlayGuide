fa5e08bb9c58a7d92e92dc0fd4c8a06590f8a406
Harley <ftauth@gmail.com>
2023-04-11 21:39:18 +0800

    [Harleythetech]Added design

--- home.cs
+++ home.cs
@@ -24,8 +24,13 @@ namespace PlayGuide
             // Create your application here
             var urbanistfont = Typeface.CreateFromAsset(Assets, "fonts/Urbanist.ttf");
             TextView tx1 = FindViewById<TextView>(Resource.Id.textView1);
+            TextView tx2 = FindViewById<TextView>(Resource.Id.textView2);
+            TextView tx3 = FindViewById<TextView>(Resource.Id.textView3);
             tx1.Typeface = urbanistfont;
             tx1.SetTypeface(tx1.Typeface, TypefaceStyle.Bold);
+            tx2.Typeface = urbanistfont;
+            tx2.SetTypeface(tx2.Typeface, TypefaceStyle.Bold);
+            tx3.Typeface = urbanistfont;
 
             AppCompatButton abt = FindViewById<AppCompatButton>(Resource.Id.about);
             abt.Click += (sender, args) =>
