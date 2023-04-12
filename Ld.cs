cb904485906dcec6eea83e360a9e8b518ff6d466
Harley <ftauth@gmail.com>
2023-04-12 14:42:49 +0800

    r006a: Bug Fixes and some additional code - Fixed back button going to L4D page - Fixed Fonts on Home page - Added Guide & Source on GGuide Page - Fixed Margins on UI - Fixed Fonts on GGuide Pages
    Note: Rebuild if it fails to detect all the stuff added.

--- Ld.cs
+++ Ld.cs
@@ -1,5 +1,6 @@
 ﻿using Android.App;
 using Android.Content;
+using Android.Graphics;
 using Android.OS;
 using Android.Runtime;
 using Android.Views;
@@ -20,13 +21,38 @@ namespace PlayGuide
             base.OnCreate(savedInstanceState);
             SetContentView(Resource.Layout.ld);
 
-            // Create your application here
+            //Load App Font
+            var urbanistfont = Typeface.CreateFromAsset(Assets, "fonts/Urbanist.ttf");
+
+            TextView tx1 = FindViewById<TextView>(Resource.Id.textView1);
+            TextView tx2 = FindViewById<TextView>(Resource.Id.textView2);
+            TextView tx3 = FindViewById<TextView>(Resource.Id.textView3);
+            TextView tx4 = FindViewById<TextView>(Resource.Id.guide);
+            TextView tx5 = FindViewById<TextView>(Resource.Id.src);
+            TextView tx6 = FindViewById<TextView>(Resource.Id.datsrc);
+            //App Title
+            tx1.Typeface = urbanistfont;
+            tx1.SetTypeface(tx1.Typeface, TypefaceStyle.Bold);
+            //Game Title
+            tx2.Typeface = urbanistfont;
+            tx2.SetTypeface(tx2.Typeface, TypefaceStyle.Bold);
+            //Game Description
+            tx3.Typeface = urbanistfont;
+            //Guide
+            tx4.Typeface = urbanistfont;
+            tx4.SetTypeface(tx4.Typeface, TypefaceStyle.Bold);
+            //Source
+            tx5.Typeface = urbanistfont;
+            tx6.Typeface = urbanistfont;
+            tx6.SetTypeface(tx6.Typeface, TypefaceStyle.Bold);
+
+
 
             //Return Btn
             AppCompatButton ret = FindViewById<AppCompatButton>(Resource.Id.btn);
             ret.Click += (sender, args) =>
             {
-                Intent intent = new Intent(this, typeof(Ld));
+                Intent intent = new Intent(this, typeof(home));
                 StartActivity(intent);
             };
         }
--- home.cs
+++ home.cs
@@ -21,17 +21,40 @@ namespace PlayGuide
         {
             base.OnCreate(savedInstanceState);
             SetContentView(Resource.Layout.home);
-            // Create your application here
+
+            //Load App font
             var urbanistfont = Typeface.CreateFromAsset(Assets, "fonts/Urbanist.ttf");
+            //Load up Design Variables
             TextView tx1 = FindViewById<TextView>(Resource.Id.textView1);
-            TextView tx2 = FindViewById<TextView>(Resource.Id.textView2);
+            TextView tx2 = FindViewById<TextView>(Resource.Id.textView2); 
             TextView tx3 = FindViewById<TextView>(Resource.Id.textView3);
+            TextView tx4 = FindViewById<TextView>(Resource.Id.textView4);
+            TextView tx5 = FindViewById<TextView>(Resource.Id.textView5);
+            TextView tx6 = FindViewById<TextView>(Resource.Id.textView6);
+            TextView tx7 = FindViewById<TextView>(Resource.Id.end);
+            TextView tx8 = FindViewById<TextView>(Resource.Id.textView8);
+            //App Title
             tx1.Typeface = urbanistfont;
             tx1.SetTypeface(tx1.Typeface, TypefaceStyle.Bold);
+            //MLBB Title
             tx2.Typeface = urbanistfont;
             tx2.SetTypeface(tx2.Typeface, TypefaceStyle.Bold);
+            //CODM title
             tx3.Typeface = urbanistfont;
+            tx3.SetTypeface(tx3.Typeface, TypefaceStyle.Bold);
+            //CODM Description
+            tx4.Typeface = urbanistfont;
+            //L4D Title
+            tx5.Typeface = urbanistfont;
+            tx5.SetTypeface(tx5.Typeface, TypefaceStyle.Bold);
+            //L4D Description
+            tx6.Typeface = urbanistfont;
+            //end
+            tx7.Typeface = urbanistfont;
+            //MLBB Description
+            tx8.Typeface = urbanistfont;
 
+            //Logic for opening next page
             //About Page
             AppCompatButton abt = FindViewById<AppCompatButton>(Resource.Id.about);
             abt.Click += (sender, args) =>
